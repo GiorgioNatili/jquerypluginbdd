a20ce08effcca6cbb24f0610e1802d26f2b6990d
GiorgioNatili <g.natili@gnstudio.com>
2014-04-01 14:41:18 -0400

    ui bdd sample code cleaned

--- spec/jquery.list.spec.js
+++ spec/jquery.list.spec.js
@@ -1,67 +1,87 @@
-describe("jquery.list", function() {
+describe('jquery.list', function() {
 	var textbox,
 		list;
 
 	beforeEach(function() {
+
 		jasmine.getFixtures().set('<input id="textbox" type="text" /><ul id="list" />');
 
 		this.addMatchers({
 			toContainText: function(expected) {
-				return this.actual.indexOf( expected ) >= 0;
+				return this.actual.indexOf(expected) >= 0;
 			}
 		});
 
 		textbox = $('#textbox');
+
 		list = $('#list').list({
+
 			input: textbox
+
 		});
+
 	});
 
-	describe("pressing enter in input when it's got a value", function() {
-		var testText = 'THIS IS SOME TEXT';
+	describe('pressing enter in input when it\'s got a value', function() {
+
+		var testText = 'THIS IS SOME DUMMY TEXT';
 
 		beforeEach(function() {
+
 			// create enter keypress event
 			var keypress = $.Event('keypress');
 			keypress.which = $.ui.keyCode.ENTER;
 
 			// set textbox value
-			textbox.val( testText );
+			textbox.val(testText);
 
 			// Send event
-			textbox.trigger( keypress );
+			textbox.trigger(keypress);
+
 		});
 
-		it("should add item to list", function() {
-			expect( list.children().length ).toEqual( 1 );
-		});
+		it('should add item to list', function() {
 
-		it("item should contain entered text", function() {
-			expect( list.find( 'li' ).text() ).toContainText( testText )
-		});
+			expect(list.children().length).toEqual(1);
 
-		describe("clicking remove link", function() {
-			it("should remove item", function() {
+	});
+
+		it('item should contain entered text', function() {
+
+			expect(list.find('li').text()).toContainText(testText);
+
+	});
+
+		describe('clicking remove link', function() {
+
+			it('should remove item', function() {
+
 				var click = $.Event('click');
-				list.find('li a').trigger( click );
+				list.find('li a').trigger(click);
 
-				expect( list ).toBeEmpty();
+				expect(list).toBeEmpty();
+
 			});
 		});
 	});
 
-	describe("pressing enter in input when there's no value", function() {
+	describe('pressing enter in input when there\'s no value', function() {
+
 		beforeEach(function() {
+
 			var keypress = $.Event('keypress');
 			keypress.which = $.ui.keyCode.ENTER;
 
 			textbox.val('');
 
-			textbox.trigger( keypress );
+			textbox.trigger(keypress);
+
 		});
 
-		it("should not add an item", function() {
+		it('should not add an item', function() {
+
 			expect( list ).toBeEmpty();
+
 		});
 	});
 });
