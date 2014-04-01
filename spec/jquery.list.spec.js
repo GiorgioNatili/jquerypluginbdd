0ff71edd6ac9af0f5ba41ea7690c3563a849dc80
GiorgioNatili <g.natili@gnstudio.com>
2014-04-01 14:44:54 -0400

    ui bdd sample code test definition

--- spec/jquery.list.spec.js
+++ spec/jquery.list.spec.js
@@ -1,12 +1,44 @@
-describe("jquery.list", function() {
+describe('jquery.list', function() {
 
+	beforeEach(function() {
 
-	describe("pressing enter in input when it's got a value", function() {
-			it('should be true', function(){
-
-				expect(true).toBeTruthy();
-
-			});
 	});
 
+	describe('pressing enter in input when it\'s got a value', function() {
+
+		beforeEach(function() {
+
+		});
+
+		it('should add item to list', function() {
+
+				
+
+		});
+
+		it('item should contain entered text', function() {
+
+
+		});
+
+		describe('clicking remove link', function() {
+
+			it('should remove item', function() {
+
+			});
+		});
+	});
+
+	describe('pressing enter in input when there\'s no value', function() {
+
+		beforeEach(function() {
+
+
+		});
+
+		it('should not add an item', function() {
+
+
+		});
+	});
 });
